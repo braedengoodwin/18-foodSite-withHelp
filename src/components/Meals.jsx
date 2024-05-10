758779e6ee3568c12ad481240fe8bc7d6787f467
braedengoodwin <braedenagoodwin@hotmail.com>
2024-05-09 20:05:15 -0500

    added alot

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,28 +1,26 @@
 import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
+
+const requestConfig = {}
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+console.log(loadedMeals)
 
-      if (!response.ok) {
-        //..
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+if(isLoading){
+  return <p>fetching meals</p>
+}
 
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <MealItem key={meal.id} meal={meal}/>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
